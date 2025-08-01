a4dfdb2cb71c8b127ffd1a44a7fe251fa236e0bf
Suryansh <suryanshyadav885@gmail.com>
2025-08-01 21:19:42 +0530

    updated socket features

--- public/main.js
+++ public/main.js
@@ -12,6 +12,7 @@ messageForm.addEventListener('submit', (e)=>{
 })
 
 function sendMessage(){
+    if(messageInput.value === '')return;
     console.log(messageInput.value)
     const data = {
         name : nameInput.value,
@@ -19,12 +20,66 @@ function sendMessage(){
         dateTime : new Date()
     }
     socket.emit('message', data);
+    addMessageToUI(true,data);
+    messageInput.value = '';
 }
 
+function addMessageToUI(isOwnMessage, data){
+    clearFeedback()
+    const element = 
+        `
+            <li class="${isOwnMessage ? "message-right"  : "message-left"}">
+                <p class="message">
+                    ${data.message}
+                    <span>${data.name} ● ${moment(data.dateTime)}</span>
+                </p>
+            </li>
+        `
+    messageContainer.innerHTML += element
+    scrollBottom();
+}
 socket.on('chat-message', (data)=>{
-    console.log(data);
+    // console.log(data);
+    addMessageToUI(false, data);
 })
 
 socket.on('clients-total',(data)=>{
     totalClients.innerText = `Total clients : ${data}`;
 })
+
+function scrollBottom(){
+    messageContainer.scrollBy(0, messageContainer.scrollHeight);
+}
+
+messageInput.addEventListener('focus', (e)=>{
+    socket.emit('feedback', {
+        feedback : `${nameInput.value} is typing`
+    })
+})
+messageInput.addEventListener('keypress', (e)=>{
+    socket.emit('feedback', {
+        feedback : `${nameInput.value} is typing`
+    })
+})
+messageInput.addEventListener('blur', (e)=>{
+    socket.emit('feedback', {
+        feedback : ''
+    })
+})
+
+socket.on('feedback', (data)=>{
+    clearFeedback()
+    const element =
+    `
+        <li class="message-feedback">
+        <p class="feedback" id="feedback">${data.feedback}</p>
+        </li>    
+    `
+    messageContainer.innerHTML += element;
+})
+
+function clearFeedback(){
+    document.querySelectorAll('li.message-feedback').forEach(element=>{
+        element.parentNode.removeChild(element);
+    })
+}
